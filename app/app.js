0622285404bc13951d227747ce0e234023913eb9
Yahya-Dalbah <yahya.dalbah@harri.com>
2025-07-10 09:55:18 +0300

    sat up the colors, icons, font-size and font-family

--- app/app.js
+++ app/app.js
@@ -6,8 +6,7 @@ myApp.config([
   function ($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-      .when("/", { templateUrl: "./views/home.html" })
-      .when("/countries", {
+      .when("/", {
         templateUrl: "./views/list.html",
         controller: "listController",
       })
